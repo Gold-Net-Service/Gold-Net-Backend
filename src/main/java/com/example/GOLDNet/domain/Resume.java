230e7d7be9fb5340208dd5f43781ffb3e396bb8f
jyun27 <kimjyun27@gmail.com>
2025-08-26 18:05:09 +0900

    feat: resume 조회, 수정 api

--- src/main/java/com/example/GOLDNet/domain/Resume.java
+++ src/main/java/com/example/GOLDNet/domain/Resume.java
@@ -2,9 +2,12 @@ package com.example.GOLDNet.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Table(name = "resumes")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Resume {
@@ -46,4 +49,40 @@ public class Resume {
 
     @Column(length = 255)
     private String preferentialTreatment; //취업 우대사항(국가유공자 자녀 등)
+
+
+    @Builder
+    public Resume(Member member, String education, String experience,
+                  String preferredConditions, String selfIntroduction, String skills,
+                  String strengths, String mbti, String licensesAbilities,
+                  String portfolioUrls, String preferentialTreatment) {
+        this.member = member;
+        this.education = education;
+        this.experience = experience;
+        this.preferredConditions = preferredConditions;
+        this.selfIntroduction = selfIntroduction;
+        this.skills = skills;
+        this.strengths = strengths;
+        this.mbti = mbti;
+        this.licensesAbilities = licensesAbilities;
+        this.portfolioUrls = portfolioUrls;
+        this.preferentialTreatment = preferentialTreatment;
+    }
+
+    // 업데이트 메서드
+    public void updateResume(String education, String experience, String preferredConditions,
+                             String selfIntroduction, String skills, String strengths,
+                             String mbti, String licensesAbilities, String portfolioUrls,
+                             String preferentialTreatment) {
+        this.education = education;
+        this.experience = experience;
+        this.preferredConditions = preferredConditions;
+        this.selfIntroduction = selfIntroduction;
+        this.skills = skills;
+        this.strengths = strengths;
+        this.mbti = mbti;
+        this.licensesAbilities = licensesAbilities;
+        this.portfolioUrls = portfolioUrls;
+        this.preferentialTreatment = preferentialTreatment;
+    }
 }
--- src/main/java/com/example/GOLDNet/repository/ResumeRepository.java
+++ src/main/java/com/example/GOLDNet/repository/ResumeRepository.java
@@ -1,7 +1,11 @@
 package com.example.GOLDNet.repository;
 
+import com.example.GOLDNet.domain.Member;
 import com.example.GOLDNet.domain.Resume;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ResumeRepository extends JpaRepository<Resume, Integer> {
+    Optional<Resume> findResumeByMember(Member member);
 }
